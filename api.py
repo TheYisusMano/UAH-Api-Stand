236ead606016e187a365d16216413cb2faa17b5c
Jesús Escobar <72752896+TheYisusMano@users.noreply.github.com>
2025-03-25 21:23:28 -0400

    añadir cors

--- api.py
+++ api.py
@@ -8,12 +8,16 @@ from flask_jwt_extended import JWTManager
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("POSTGRE_KEY")
 
 app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+CORS(app)
+
 jwt = JWTManager(app)
 
 
